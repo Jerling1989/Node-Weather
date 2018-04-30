38693432d7c179d0a7909e379e529149c3709020
Jacob <jerling1989@gmail.com>
2018-04-30 00:16:22 -0400

    print entire object

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
 	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyA4_k5Tm12dl2-g-JOUP2YQuDUD1GynHaI',
 	json: true
 }, (error, response, body) => {
-	console.log(body);
+	console.log(JSON.stringify(body, undefined, 2));
 });
